7b56ee2093534522b3468ca3ba1fccf8e3ba477d
g127kato <87055187+g127kato@users.noreply.github.com>
2021-07-14 13:11:41 +0900

    x < 0 -> !(x >= 0)

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。
 {
   double y;
-  if( x < 0 ){ //sqrt関数は0より小さい数はエラーとなる。
+  if( !(x >= 0) ){ //0以上の数はエラーとなる。
     exit(1);
   }
   y = sqrt(x);
